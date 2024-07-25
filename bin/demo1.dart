ee04a1f555d987bf45f206dbaab024d9f1240c55
Milan Poudel <poudelmilan59@gmail.com>
2024-07-25 12:41:18 +0545

    Starting dart basic

---
+++ bin/demo1.dart
@@ -0,0 +1,23 @@
+import 'package:demo1/demo1.dart' as demo1;
+
+void main(List<String> arguments) {
+ String name ="My name is Milan poudel";
+ print(name);
+ // Write a program in Dart that finds simple interest. Formula= (p * t * r) / 100
+
+ int p=100,t=23,r=20;
+ double i;
+ i=(p* t *r)/100;
+ print(i); 
+
+
+ List<int> number=[ 1,2,3,4,5,6];
+ print(number[3]);
+
+ number.insert(6, 9865709091);
+ print(number);
+
+ 
+
+
+}
